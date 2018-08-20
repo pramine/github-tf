72bb56e41591a7d9a60b82aeda8884bc224548e6
Luke Kysow <lkysow@gmail.com>
2018-08-20 13:47:10 -1000

    Initial commit

---
+++ main.tf
@@ -0,0 +1,45 @@
+provider "github" {
+  organization = "runatlantis"
+}
+
+# The runatlantis/github-tf repository. This is actually
+# where this file lives. This repo holds the Terraform
+# for managing the rest of the GitHub organization.
+resource "github_repository" "github_tf" {
+  name        = "github-tf"
+  description = "Manages the GitHub users for the runatlantis organizatio"
+}
+
+# The team that has permissions to manage the runatlantis/github-tf.
+resource "github_team" "github_tf_writers" {
+  name        = "github-tf-writers"
+  description = "github-tf-writers"
+  privacy     = "closed"
+}
+
+# Give the github-tf-writers team permissions to write to the runatlantis/github-tf
+# repo.
+resource "github_team_repository" "github_tf_writers" {
+  team_id    = "${github_team.github_tf_writers.id}"
+  repository = "${github_repository.github_tf.name}"
+  permission = "push"
+}
+
+variable newhire_username {
+  default = "newhire"
+}
+
+# Operator adds the newhire to the organization so they
+# can create their own pull request.
+resource "github_membership" "newhire_org_membership" {
+  username = "${var.newhire_username}"
+  role     = "admin" # todo: change to member
+}
+
+# Operator adds the new hire to the team with write permissions
+# to the github organization repo.
+resource "github_team_membership" "newhire_tf_writers" {
+  team_id  = "${github_team.github_tf_writers.id}"
+  username = "${var.newhire_username}"
+  role     = "maintainer" # todo: change to member
+}
